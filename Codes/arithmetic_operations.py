9f93a64abe2eef4894c4949c4c977f450b97b4bd
MidhaTahir <midhatahirkhan2011@gmail.com>
2020-03-23 19:56:46 +0500

    adding arithmetic_program

---
+++ Codes/arithmetic_operations.py
@@ -0,0 +1,16 @@
+print(3+5)
+print("----")
+print(1-2)
+print("----")
+print(8*2+5)
+print("----")
+print(8/2*2+2-4) #BODMAS
+print("----")
+print((5+9)-(8-1)) #BODMAS
+print("----")
+print(6%2) #remainder
+print("----")
+print(5//2) #integer division
+print("----")
+print(2**2) #power 
+print("----")
